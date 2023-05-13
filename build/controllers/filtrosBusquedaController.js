f1ca3ea33d9fdaaee159f3ce5a400f8ca718f8e9
Sebastian Zuluaga <jhoanzh21@gmail.com>
2023-05-13 09:14:36 -0500

    Creacion de filtros

--- build/controllers/filtrosBusquedaController.js
+++ build/controllers/filtrosBusquedaController.js
@@ -14,11 +14,39 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const database_1 = __importDefault(require("../database"));
 class FiltrosBusqueda {
-    search(req, res) {
+    searchFicha(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             const { id } = req.params;
-            const fichasId = yield database_1.default.promise().query("SELECT * FROM fichas WHERE code_ficha = ?", [id]);
+            const fichasId = yield database_1.default.promise().query("SELECT * FROM fichas WHERE code_ficha like ? or code_ficha like ? or name_ficha like ?", [id + '%', '%' + id, id + '%']);
             res.json(fichasId);
+        });
+    }
+    searchUsuario(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { filtro } = req.params;
+            const usuariosId = yield database_1.default.promise().query("SELECT * FROM usuarios WHERE num_id like ? or email like ? or first_name like ? or last_name like ?", [filtro + '%', '%' + filtro, filtro + '%', '%' + filtro]);
+            res.json(usuariosId);
+        });
+    }
+    searchHorario(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { filter } = req.params;
+            const horarioId = yield database_1.default.promise().query("SELECT * FROM horario WHERE id_horario like ? ", [filter + '%']);
+            res.json(horarioId);
+        });
+    }
+    searchAsistencia(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { filter } = req.params;
+            const asistenciaId = yield database_1.default.promise().query("SELECT * FROM asistencias WHERE id_aprendiz like ? ", [filter + '%']);
+            res.json(asistenciaId);
+        });
+    }
+    searchExcusa(req, res) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const { filter } = req.params;
+            const excusaId = yield database_1.default.promise().query("SELECT * FROM asistencias WHERE id_asistencia like ? ", [filter + '%']);
+            res.json(excusaId);
         });
     }
 }
--- src/controllers/filtrosBusquedaController.ts
+++ src/controllers/filtrosBusquedaController.ts
@@ -3,12 +3,39 @@ import db from '../database';
 
 class FiltrosBusqueda{
 
-public async search(req:Request,res:Response):Promise<any>{
+public async searchFicha(req:Request,res:Response):Promise<any>{
     const {id} = req.params;
-    const fichasId = await db.promise().query("SELECT * FROM fichas WHERE code_ficha = ?",[id]);
+    const fichasId = await db.promise().query("SELECT * FROM fichas WHERE code_ficha like ? or code_ficha like ? or name_ficha like ?",[id+'%','%' + id , id+'%']);
     res.json(fichasId);
 }
+
+public async searchUsuario(req:Request,res:Response):Promise<any>{
+    const {filtro} = req.params;
+    const usuariosId = await db.promise().query("SELECT * FROM usuarios WHERE num_id like ? or email like ? or first_name like ? or last_name like ?",[filtro+'%','%' + filtro , filtro+'%' , '%' + filtro]);
+    res.json(usuariosId);
 }
 
+public async searchHorario(req:Request,res:Response):Promise<any>{
+    const {filter} = req.params;
+    const horarioId = await db.promise().query("SELECT * FROM horario WHERE id_horario like ? ",[filter+'%']);
+    res.json(horarioId);
+}
+
+public async searchAsistencia(req:Request,res:Response):Promise<any>{
+    const {filter} = req.params;
+    const asistenciaId = await db.promise().query("SELECT * FROM asistencias WHERE id_aprendiz like ? ",[filter+'%']);
+    res.json(asistenciaId);
+}
+
+public async searchExcusa(req:Request,res:Response):Promise<any>{
+    const {filter} = req.params;
+    const excusaId = await db.promise().query("SELECT * FROM asistencias WHERE id_asistencia like ? ",[filter+'%']);
+    res.json(excusaId);
+}
+
+
+
+}
+ 
 const filtrosBusqueda = new FiltrosBusqueda();
 export default filtrosBusqueda;
