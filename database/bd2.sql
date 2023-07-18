6f0af23da56dcb092f30cd4e803e7051b8781a20
Juan Guillermo <g.duque@utp.edu.co>
2023-07-18 09:54:47 -0500

    cambios bd

--- database/bd2.sql
+++ database/bd2.sql
@@ -31,9 +31,9 @@ CREATE TABLE componentes_roles(
 
 INSERT INTO componentes_roles(id_rol , id_componente) values (1,1),(1,2),(1,3),(1,4),(1,5),
 (1,6);
-INSERT INTO componentes_roles(id_rol , id_componente) values (2,1),(2,2),(2,3),(2,4),(2,5);
+INSERT INTO componentes_roles(id_rol , id_componente) values (2,1),(2,2),(2,3),(2,4);
 
-INSERT INTO componentes_roles(id_rol , id_componente) values (3,7);
+INSERT INTO componentes_roles(id_rol , id_componente) values (3,7),(3,8);
 
 CREATE TABLE usuarios( 
     num_id BIGINT UNSIGNED UNIQUE NOT NULL PRIMARY KEY,
